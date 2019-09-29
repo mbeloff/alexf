7dd8dd8ad0c40f8b6a569622801c9ab4d7724b56
Michael Beloff <michael.a.beloff@gmail.com>
2019-09-29 12:41:09 +1000

    content

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+	
 	Waves.attach('.btn, .btn-floating', ['waves-light']);
 	// Waves.attach('.view .mask', ['waves-light']);
 	Waves.attach('.waves-light', ['waves-light']);
